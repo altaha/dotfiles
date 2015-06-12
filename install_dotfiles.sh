034bb8cdda40c389f78485db3a08c209aa2f87d1
Ahmed Altaha <altaha@Ahmeds-MacBook-Pro-2.local>
2015-06-12 12:52:58 -0400

    Add a hacky dotfiles install script

---
+++ install_dotfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Molokai colorscheme
+mkdir -p ~/.vim/colors
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+mv molokai.vim ~/.vim/colors
+
+#Install files
+SRC_FILE=.bash_profile
+DEST_FILE=~/.bash_profile
+if [ -f $DEST_FILE ];
+then
+   echo "File $DEST_FILE exists."
+else
+  cp $SRC_FILE $DEST_FILE
+fi
+
+SRC_FILE=.vimrc
+DEST_FILE=~/.vimrc
+if [ -f $DEST_FILE ];
+then
+   echo "File $DEST_FILE exists."
+else
+  cp $SRC_FILE $DEST_FILE
+fi
+
+SRC_FILE=.tmux.conf
+DEST_FILE=~/.tmux.conf
+if [ -f $DEST_FILE ];
+then
+   echo "File $DEST_FILE exists."
+else
+  cp $SRC_FILE $DEST_FILE
+fi
